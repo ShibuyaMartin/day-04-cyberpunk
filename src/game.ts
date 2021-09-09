fee785bf46fd8c1f9f0fce035923b799ef6057f6
Shibu <shibu@decentraland.org>
2021-09-08 21:39:12 -0300

    fix screens

--- src/game.ts
+++ src/game.ts
@@ -7,7 +7,21 @@ metaverse.addComponent(
   new Transform({
     position: new Vector3(48, 0, 64),
     rotation: Quaternion.Euler(0, 270, 0),
-    scale: new Vector3(1,1,1)
+    scale: new Vector3(1, 1, 1)
   })
 )
-engine.addEntity(metaverse) 
+engine.addEntity(metaverse)
+
+
+//add fewo
+
+export let fewo = new Entity()
+fewo.addComponent(new GLTFShape('models/fewo.glb'))
+fewo.addComponent(
+  new Transform({
+    position: new Vector3(15, -18, 72),
+    rotation: Quaternion.Euler(0, 90, 0),
+    scale: new Vector3(3, 3, 3)
+  })
+)
+engine.addEntity(fewo)
