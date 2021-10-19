9b66bb10d80a43f751428e7f752956f7549d8e79
Lastraum <lastraumonline@gmail.com>
2021-10-18 17:39:16 -0700

    finished adding video screens

--- src/resources.ts
+++ src/resources.ts
@@ -1,17 +1,32 @@
 export default{
 
-        screens:[
-            {transform:{position: new Vector3(83.3,36.1,97.2), rotation: Quaternion.Euler(0,270,0), scale: new Vector3(24,9,1)}, mask:false, uvs: [0.033, 0.1907, 0.3, 0.1907, 0.3, 0.0129, 0.033, 0.0129, 0, 0, 0, 0, 0, 0, 0, 0]},
-            {transform:{position: new Vector3(90.8,38.3,64), rotation: Quaternion.Euler(0,270,0), scale: new Vector3(22.6,6,1)}, mask:false, uvs: [0.3645, 0.1907, 0.6312, 0.1907, 0.6312, 0.0129, 0.3465, 0.0129, 0, 0, 0, 0, 0, 0, 0, 0]},
-            {transform:{position: new Vector3(83.3,36.1,30.4), rotation: Quaternion.Euler(0,270,0), scale: new Vector3(24,9,1)}, mask:false, uvs: [0.7, 0.1907, 0.9666, 0.1907, 0.9666, 0.0129, 0.7, 0.0129, 0, 0, 0, 0, 0, 0, 0, 0]},
-            {transform: {position: new Vector3(96.9,19.5,64), rotation: Quaternion.Euler(0,270,0), scale: new Vector3(27,27,1)}, mask:true, uvs: [0.2968, 0.9351, 0.7020, 0.9351, 0.7020, 0.2148, 0.2968, 0.2148, 0, 0, 0, 0, 0, 0, 0, 0]},
-            {transform:{position: new Vector3(80,19,98.1), rotation: Quaternion.Euler(0,244,0), scale: new Vector3(6.9,17.9,1)}, mask:false, uvs: [0.1896, 0.7537, 0.2916, 0.7537, 0.2916, 0.2537, 0.1896, 0.2537, 0, 0, 0, 0, 0, 0, 0, 0]},
-            {transform:{position: new Vector3(80,19,29.48), rotation: Quaternion.Euler(0,296,0), scale: new Vector3(6.9,17.9,1)}, mask:false, uvs: [0.7052, 0.7537, 0.8093, 0.7537, 0.8093, 0.2537, 0.7052, 0.2537, 0,0,0,0,0,0,0,0]},
-            {transform:{position: new Vector3(71.5,25.2,106.2), rotation: Quaternion.Euler(0,231,0), scale: new Vector3(11.3,4.1,1)}, mask:false, uvs: [0.0052, 0.3685, 0.1823, 0.3685, 0.1823, 0.2537, 0.0052, 0.2537, 0, 0, 0, 0, 0, 0, 0, 0]},
-            {transform:{position: new Vector3(71.5,19.4,106.2), rotation: Quaternion.Euler(0,231,0), scale: new Vector3(11.3,4.2,1)}, mask:false, uvs: [0.0052, 0.4833, 0.1823, 0.4833, 0.1823, 0.3704, 0.0052, 0.3704, 0, 0, 0, 0, 0, 0, 0, 0]},
-            {transform:{position: new Vector3(71.5,25.2,21.32), rotation: Quaternion.Euler(0,309,0), scale: new Vector3(11.3,4.2,1)}, mask:false, uvs: [0.8166, 0.3685, 0.9937, 0.3685, 0.9937, 0.2537, 0.8166, 0.2537, 0, 0, 0, 0, 0, 0, 0, 0]},
-            {transform:{position: new Vector3(71.5,19.4,21.32), rotation: Quaternion.Euler(0,309,0), scale: new Vector3(11.3,4.2,1)}, mask:false, uvs: [0.8166, 0.4833, 0.9937, 0.4833, 0.9937, 0.3704, 0.8166, 0.3704, 0, 0, 0, 0, 0, 0, 0, 0]},
+    screens:[
+        {transform:{position: new Vector3(83.3,36.1,97.2), rotation: Quaternion.Euler(0,270,0), scale: new Vector3(24,9,1)}, mask:false, uvs: [0.033, 0.1907, 0.3, 0.1907, 0.3, 0.0129, 0.033, 0.0129, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(90.8,38.3,64), rotation: Quaternion.Euler(0,270,0), scale: new Vector3(22.6,6,1)}, mask:false, uvs: [0.3645, 0.1907, 0.6312, 0.1907, 0.6312, 0.0129, 0.3465, 0.0129, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(83.3,36.1,30.4), rotation: Quaternion.Euler(0,270,0), scale: new Vector3(24,9,1)}, mask:false, uvs: [0.7, 0.1907, 0.9666, 0.1907, 0.9666, 0.0129, 0.7, 0.0129, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform: {position: new Vector3(96.9,19.5,64), rotation: Quaternion.Euler(0,270,0), scale: new Vector3(27,27,1)}, mask:true, uvs: [0.2968, 0.9351, 0.7020, 0.9351, 0.7020, 0.2148, 0.2968, 0.2148, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(80,19,98.1), rotation: Quaternion.Euler(0,244,0), scale: new Vector3(6.9,17.9,1)}, mask:false, uvs: [0.1896, 0.7537, 0.2916, 0.7537, 0.2916, 0.2537, 0.1896, 0.2537, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(80,19,29.48), rotation: Quaternion.Euler(0,296,0), scale: new Vector3(6.9,17.9,1)}, mask:false, uvs: [0.7052, 0.7537, 0.8093, 0.7537, 0.8093, 0.2537, 0.7052, 0.2537, 0,0,0,0,0,0,0,0]},
+        {transform:{position: new Vector3(71.5,25.2,106.2), rotation: Quaternion.Euler(0,231,0), scale: new Vector3(11.3,4.1,1)}, mask:false, uvs: [0.0052, 0.3685, 0.1823, 0.3685, 0.1823, 0.2537, 0.0052, 0.2537, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(71.5,19.4,106.2), rotation: Quaternion.Euler(0,231,0), scale: new Vector3(11.3,4.2,1)}, mask:false, uvs: [0.0052, 0.4833, 0.1823, 0.4833, 0.1823, 0.3704, 0.0052, 0.3704, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(71.5,25.2,21.32), rotation: Quaternion.Euler(0,309,0), scale: new Vector3(11.3,4.2,1)}, mask:false, uvs: [0.8166, 0.3685, 0.9937, 0.3685, 0.9937, 0.2537, 0.8166, 0.2537, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(71.5,19.4,21.32), rotation: Quaternion.Euler(0,309,0), scale: new Vector3(11.3,4.2,1)}, mask:false, uvs: [0.8166, 0.4833, 0.9937, 0.4833, 0.9937, 0.3704, 0.8166, 0.3704, 0, 0, 0, 0, 0, 0, 0, 0]},
 
-        ]
+        {transform:{position: new Vector3(82.5,23.9,88.03), rotation: Quaternion.Euler(13.98,258.092,359.748), scale: new Vector3(9,7.3,1)}, mask:false, uvs: [0.0052, 0.7519, 0.1823, 0.7519, 0.1823, 0.5019, 0.0052, 0.5019, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(82.58,23.9,39.53), rotation: Quaternion.Euler(15.58,281.755,0.303), scale: new Vector3(9,7.3,1)}, mask:false, uvs: [0.8166, 0.7519, 0.9937, 0.7519, 0.9937, 0.5019, 0.8166, 0.5019, 0, 0, 0, 0, 0, 0, 0, 0]},
+
+        {transform:{position: new Vector3(44.8,11,82.4), rotation: Quaternion.Euler(0,129,0), scale: new Vector3(11.2,6.3,1)}, mask:false, uvs: [0.0646, 0.9907, 0.2948, 0.9907, 0.2948, 0.7666, 0.0646, 0.7666, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform:{position: new Vector3(44.8,11,45.2), rotation: Quaternion.Euler(0,51,0), scale: new Vector3(11.2,6.3,1)}, mask:false, uvs: [0.0646, 0.9907, 0.2948, 0.9907, 0.2948, 0.7666, 0.0646, 0.7666, 0, 0, 0, 0, 0, 0, 0, 0]},
+
+        {transform:{position: new Vector3(44.8,26,82.4), rotation: Quaternion.Euler(0,129,0), scale: new Vector3(11.2,6.3,1)}, mask:false, uvs: [0.7052, 0.9907, 0.2948, 0.9364, 0.2948, 0.9364, 0.7052, 0.7666, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform: {position: new Vector3(44.8,26,45.2), rotation: Quaternion.Euler(0,51,0), scale: new Vector3(11.2,6.3,1)}, mask:false, uvs: [0.7052, 0.9907, 0.2948, 0.9364, 0.2948, 0.9364, 0.7052, 0.7666, 0, 0, 0, 0, 0, 0, 0, 0]},
+
+
+
+        {transform: {position: new Vector3(70.3,8.5,105), rotation: Quaternion.Euler(0,226,0), scale: new Vector3(10,10,1)}, mask:true, uvs: [0.2968, 0.9351, 0.7020, 0.9351, 0.7020, 0.2148, 0.2968, 0.2148, 0, 0, 0, 0, 0, 0, 0, 0]},
+        {transform: {position: new Vector3(70.1,8.5,22.4), rotation: Quaternion.Euler(0,313,0), scale: new Vector3(10,10,1)}, mask:true, uvs: [0.2968, 0.9351, 0.7020, 0.9351, 0.7020, 0.2148, 0.2968, 0.2148, 0, 0, 0, 0, 0, 0, 0, 0]},
+
+
+    ]
 }
 
